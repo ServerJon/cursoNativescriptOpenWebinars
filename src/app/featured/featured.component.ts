854af200a9f4d17d6b6eee490de2a7ccf07c7e60
ServerJon <ctsesiphon@hotmail.com>
2021-05-01 18:09:24 +0200

    feat(03_Plugins): Añadida funcionalidad cámara

--- src/app/featured/featured.component.ts
+++ src/app/featured/featured.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core'
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer'
 import { Application } from '@nativescript/core'
+import { Image } from '@nativescript/core'
+
+import * as camera from "@nativescript/camera";
 
 @Component({
   selector: 'Featured',
   templateUrl: './featured.component.html',
+  styles: [`
+      Button {
+          background-color: blue;
+          color: white;
+      }
+  `]
 })
 export class FeaturedComponent implements OnInit {
+
+  public imagePhoto: Image;
+
   constructor() {
     // Use the component constructor to inject providers.
+
+    this.imagePhoto = new Image();
+    this.imagePhoto.src = "res://logoopenwebinars";
   }
 
   ngOnInit(): void {
@@ -18,5 +33,17 @@ export class FeaturedComponent implements OnInit {
   onDrawerButtonTap(): void {
     const sideDrawer = <RadSideDrawer>Application.getRootView()
     sideDrawer.showDrawer()
+  }
+
+  public onTap(): void {
+    camera.requestPermissions().then(
+        () => {
+            camera.takePicture().then(
+                (imageAsset) => {
+                    this.imagePhoto.src = imageAsset;
+                }
+            )
+        }
+    );
   }
 }
